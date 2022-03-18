a046d3f05793bd09055cb94f2cf32251f37afcfb
ColinQiu7 <965024754@qq.com>
2022-03-18 21:03:07 +0800

    First

---
+++ Assets/Script/zoomController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomController : MonoBehaviour
+{
+    float zoomsize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zoomsize = 0.001f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sizeChange();
+    }
+
+    void sizeChange()
+    {
+        transform.localScale = transform.localScale + zoomsize * transform.localScale;
+
+        if(transform.localScale.x >= 3.2f)
+        {
+            transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        }
+    }
+}
