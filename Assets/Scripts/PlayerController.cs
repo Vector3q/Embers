f10fdb68d87cc6918de60f962201a16fa3b86224
qiugeng17 <101917289+qiugeng17@users.noreply.github.com>
2022-04-06 16:46:24 +0800

    map update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,49 @@ public class PlayerController : MonoBehaviour
 
     bool isOpen;
 
-    //Animator animator;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    Rigidbody2D rigidbody2d;
+   
     Vector3 movement;
+
+    float horizontal;
+    float vertical;
 
     void Start()
     {
-        //animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed, 0);
+        //movement = new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed, 0);
 
-        transform.Translate(movement);//移动
+        //transform.Translate(movement);//移动
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("MoveX", lookDirection.x);
+        animator.SetFloat("MoveY", lookDirection.y);
+        animator.SetFloat("running", move.magnitude);
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
 
         //if (movement != Vector3.zero)//动画
         //{
@@ -41,6 +70,14 @@ public class PlayerController : MonoBehaviour
 
         OpenMyBag();
     }
+    /*void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }*/
 
     void OpenMyBag()
     {
