5bead7173655ead2abb8e0c130f3054093288217
Cutie-Chen <2418707510@qq.com>
2022-04-19 22:42:35 +0800

    拾取变化

--- Assets/Scripts/change.cs
+++ Assets/Scripts/change.cs
@@ -6,6 +6,7 @@ public class change : MonoBehaviour
 {
     public GameObject miss;
     public GameObject obj;
+    public GameObject player;
     bool judge = true;
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,8 @@ public class change : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (judge)
-        {
+        if (Input.GetKeyDown(KeyCode.I)) { 
+        
             miss.SetActive(false);
             obj.SetActive(true);
         }
