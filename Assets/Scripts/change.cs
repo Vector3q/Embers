18992ca620f4324ef18b64f2da4b617572b8255c
Cutie-Chen <2418707510@qq.com>
2022-04-25 14:46:12 +0800

    corn

--- Assets/Scripts/change.cs
+++ Assets/Scripts/change.cs
@@ -6,7 +6,7 @@ public class change : MonoBehaviour
 {
     public GameObject miss;
     public GameObject obj;
-    bool judge = true;
+   // bool judge = true;
     // Start is called before the first frame update
     void Start()
     {
