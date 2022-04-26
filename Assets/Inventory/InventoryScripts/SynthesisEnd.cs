55a004a7dafd022457db60fc7ae4e5c7941e5e9e
ColinQiu7 <965024754@qq.com>
2022-04-26 22:15:19 +0800

    nothing important

--- Assets/Inventory/InventoryScripts/SynthesisEnd.cs
+++ Assets/Inventory/InventoryScripts/SynthesisEnd.cs
@@ -8,18 +8,15 @@ public class SynthesisEnd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(disappear());
-    }
-    public IEnumerator disappear()
-    {
-        //Thread.Sleep(2000);
-        //transform.gameObject.SetActive(false);
-        yield return new WaitForSeconds(0);
         
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.anyKeyDown)
+        {
+            transform.gameObject.SetActive(false);
+        }
     }
 }
