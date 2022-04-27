c0f8881f5ab8aa461dd1365221eb80f9fb31d982
ThePrettyMoon <1641974090@qq.com>
2022-04-27 18:17:11 +0800

    EnemyBeast受伤和移动AI设定部分完成

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,12 +7,19 @@ public abstract class Enemy : MonoBehaviour
     //所有enemy都具有的特性，伤害和血量
     public int health;
     public int Attackdamage;
+    //受到攻击之后闪烁红色需要的变量数据
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
 
+    //受到攻击之后闪烁的时间
+    public float flashTime;
 
     // Start is called before the first frame update
     public void Start()
     {
-
+        //程序开始的时候默认获取原来的颜色信息（一般是白色）
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
@@ -25,10 +32,24 @@ public abstract class Enemy : MonoBehaviour
         }
     }
 
+    //受到伤害之后扣除血量和闪烁
     public void TakeDamage(int damage)
     {
         health -= damage;
+        flashColor(flashTime);
     }
 
+    //受击闪烁函数
+    void flashColor(float time)
+    {
+        spriteRenderer.color = Color.red;
+        //time时间之后调用resetcolor方法
+        Invoke("resetColor", time);
+    }
 
+    //恢复原本的颜色
+    void resetColor()
+    {
+        spriteRenderer.color = originalColor;
+    }
 }
--- Assets/Scripts/Enemy/EnemyBeast.cs
+++ Assets/Scripts/Enemy/EnemyBeast.cs
@@ -4,18 +4,62 @@ using UnityEngine;
 
 public class EnemyBeast : Enemy
 {
+    //老虎的AI设定：一直以直线最短路径向玩家移动
+    //老虎受到攻击之后会被击退并且原地眩晕一下（被击退并且僵直）
 
+
+    public float Speed;
+    public float AttackCD;
+    Rigidbody2D mrigidbody2D;
+
+    //老虎的移动目标坐标:玩家
+    Vector2 moveTarget;
+    //老虎的位置
+    Vector2 position;
 
     // Start is called before the first frame update
     public void Start()
     {
         base.Start();
+        mrigidbody2D = GetComponent<Rigidbody2D>();
+/*        StartCoroutine(Wake());*/
     }
 
     // Update is called once per frame
     public void Update()
     {
         base.Update();
+        //获取玩家的位置
+        GetPlayerPosition();
+        //向玩家移动
+        MoveToPlayer();
+        
     }
 
+    private void GetPlayerPosition()
+    {
+        moveTarget.x = GameObject.FindWithTag("Player").GetComponent<Transform>().position.x;
+        moveTarget.y = GameObject.FindWithTag("Player").GetComponent<Transform>().position.y;
+    }
+
+    void MoveToPlayer()
+    {
+        Vector2 MoveDirection;
+        //获取运动的方向
+        MoveDirection.x=moveTarget.x-transform.position.x;
+        MoveDirection.y=moveTarget.y-transform.position.y;
+        //运动向量归一化
+        MoveDirection.Normalize();
+
+        //向玩家移动
+        moveTarget.x=transform.position.x+MoveDirection.x*Speed*Time.deltaTime;
+        moveTarget.y=transform.position.y+MoveDirection.y*Speed*Time.deltaTime;
+        mrigidbody2D.MovePosition(moveTarget);
+
+    }
+
+    IEnumerator Wake()
+    {
+        yield return new WaitForSeconds(5);
+    }
 }
